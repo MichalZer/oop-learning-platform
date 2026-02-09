deb4d5075e6f3f97a0dc27728ebf6641a6c7b93f
Elinor12w <elinor12w@gmail.com>
2026-02-09 16:43:44 +0200

    fixing the name in the auth header

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,20 +3,20 @@ import jwt from 'jsonwebtoken';
 export default function auth(req, res, next) {
 
     // Get the Authorization header from the request
-    const autHaeder = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!autHaeder||!authHaeder.startsWith("Bearer ")) {
+    if (!authHeader||!authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ message: "Unauthorized - No token provided" });
 }
 try {
     //Extract the token part from the header
-    const token = authHaeder.split (" ")[1];
+    const token = authHeader.split (" ")[1];
 
     // Verify the token using the secret key
-    const secoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Attach the user information from the token to the request object
-    req.user = secoded;
+    req.user = decoded;
 
     // Proceed to the next middleware or route handler
     next();
