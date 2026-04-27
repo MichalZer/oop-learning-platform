bf3dd3948f4e7495aaffaf8b505da73846b6e1ad
Elinor <elinor12w@gmail.com>
2026-04-27 12:36:00 +0300

    Update API routes for production monolith

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ if (fs.existsSync(frontendPath)) {
   app.use(express.static(frontendPath));
 
   app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
+    res.sendFile(path.join(frontendPath, 'index.html'));
+  });
 } else {
   console.warn(
     `Frontend build directory not found. Static files are disabled. Checked: ${clientDistPath} and ${clientBuildPath}`
